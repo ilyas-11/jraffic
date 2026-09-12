548c5e9532a0e5c4d2d99c601db076059964d9b9
Ilyass Abid <ilyassabid11@gmail.com>
2026-09-12 13:05:37 +0100

    fix queue and car cleanup

--- src/core/World.java
+++ src/core/World.java
@@ -3,9 +3,19 @@ package core;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import traffic.Intersection;
 
 public class World {
+    private final Intersection intersection;
     private final List<Vehicle> vehicles = new ArrayList<>();
+
+    public World(Intersection intersection) {
+        this.intersection = intersection;
+    }
+
+    public Intersection getIntersection() {
+        return intersection;
+    }
 
     public void addVehicle(Vehicle vehicle) {
         vehicles.add(vehicle);
@@ -13,6 +23,13 @@ public class World {
 
     public List<Vehicle> getVehicles() {
         return Collections.unmodifiableList(vehicles);
+    }
+
+    public void update(List<Vehicle> permittedVehicles, double frameScale) {
+        for (Vehicle vehicle : permittedVehicles) {
+            vehicle.move(frameScale);
+        }
+        vehicles.removeIf(Vehicle::isFinished);
     }
 
     public void clear() {
