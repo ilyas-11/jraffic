d3572c2e331d883f472c62494ead313dc726f1f7
Zakaria Bourazza <zakinirvana482@gmail.com>
2026-09-12 15:41:09 +0100

    handle light switching

--- src/traffic/TrafficManager.java
+++ src/traffic/TrafficManager.java
@@ -1,24 +1,35 @@
 package traffic;
 
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.Random;
 import core.Direction;
+import core.Route;
 import core.Vehicle;
 import core.World;
 
 public class TrafficManager {
     private final World world;
     private final Intersection intersection;
+    private final Map<Direction, TrafficLight> lights = new EnumMap<>(Direction.class);
+    private Direction active = Direction.NORTH;
 
     public TrafficManager(World world, Intersection intersection) {
         this.world = world;
         this.intersection = intersection;
+        for (Direction d : Direction.values()) {
+            lights.put(d, new TrafficLight(d));
+        }
     }
 
     public boolean spawn(Direction d) {
+        Route[] routes = Route.values();
+        Route route = routes[new Random().nextInt(routes.length)];
         world.addVehicle(new Vehicle(d, 400, 300));
         return true;
     }
 
     public TrafficLight getLight(Direction d) {
-        return new TrafficLight(d);
+        return lights.get(d);
     }
 }
